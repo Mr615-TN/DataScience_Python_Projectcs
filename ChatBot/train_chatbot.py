d320c9f96f219e578f99db936b60a1f4ca42d3f9
Tanish Neema <tneema501@gmail.com>
2024-05-05 14:40:52 -0500

    finished the trainchatbot.py and created gui chatbot.py

--- ChatBot/train_chatbot.py
+++ ChatBot/train_chatbot.py
@@ -14,3 +14,56 @@ import pickle
 
 files = open('intents.json').read()
 intents = json.loads(files)
+
+
+#Preprocess Data
+words = []
+classes = []
+documents = []
+
+ignoreLetters = ['!', '?', ',', '.']
+
+for possible_intent in intents['intents']:
+    for possible_patterns in intents['patters']:
+        word = nltk.word_tokenize(possible_patterns)
+        documents.append((word, possible_intent['tag']))
+        if possible_intent['tag'] not in classes:
+            classes.append(possible_intent['tag'])
+print(documents)
+
+#lemmatize
+words = [lemmatizer.lemmatize(w.lower()) for w in words if w not in ignoreLetters]
+words = sorted(list(set(words)))
+classes = sorted(list(set(classes)))
+
+print (len(documents), "documents")
+print (len(classes), "classes", classes)
+print (len(words), "unique lemmatized words", words)
+
+pickle.dump(words,open('words.pkl','wb'))
+pickle.dump(classes,open('classes.pkl','wb'))
+
+#train data
+training = []
+emptyArray = [0] * len(classes)
+
+for doc in documents:
+    bag_of_words = []
+    wordPatterns = doc[0]
+
+    wordPatterns = [lemmatizer.lemmatize(word.lower()) for word in wordPatterns]
+    for word in words:
+        bag_of_words.append(1) if word in wordPatterns else bag_of_words.append(0)
+    
+    row_output = list(emptyArray)
+    row_output[classes.index(doc[1])] = 1
+
+    training.append([bag_of_words, row_output])
+
+random.shuffle(training)
+training = np.array(training)
+
+x = list(training[:,0])
+y = list(training[:,1])
+
+print("Training data created")

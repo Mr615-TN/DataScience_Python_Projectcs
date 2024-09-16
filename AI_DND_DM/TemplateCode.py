27a24f81e487a25f30f9e45a34dd8f027def9d47
Mr615-TN <tneema501@gmail.com>
2024-09-15 21:39:46 -0500

    something is getting updated

--- AI_DND_DM/TemplateCode.py
+++ AI_DND_DM/TemplateCode.py
@@ -68,5 +68,46 @@ class DungeonMaster:
             class_character = input(f"Enter class for {name}: ")
             self.characters.append(Character(name, class_character, 1))
     
+    def generate_start_loc(self):
+        self.current_loc = Location("Village of Hillystone", "A small and peaceful village that is nestled in a valley")
+
+    def main_gameLoop(self):
+        while True:
+            self.describe_currentLoc()
+            action = input("What would you like to do? ").lower()
+            if action == "quit":
+                break
+            elif action == "look around":
+                self.talk_npc()
+            elif action == "fight"
+                self.initiateFight()
+            else:
+                print("I dont understand the action")
+
+    def describe_currentLoc(self):
+        print(f"\nYou are in {self.current_loc.name}.")
+        print(self.current_loc.description)
+
+    def talk_npc(self):
+        if not self.current_loc.npcs:
+            print("There is currently no one here to talk to")
+            return
+        npc = random.choice(self.current_loc.npcs)
+        print(f"You approach {npc.name}.{npc.description}")
+
+    def initiateFight(self):
+        enemies = [Character(f"Goblin {i}", "Goblin", 1) for i in range(3)]
+        allCombatants = self.characters + enemies
+        self.combat_sys.roll_order(allCombatants)
+        print("Combat has been initiated! Rolling for initiative!")
+        for character, initiative in self.combat_sys.initiativeOrder:
+            print(f"{character.name}: {initiative}")
+        self.combat_sys.process_combat_round()
+
+__name__ == "__main__":
+    dm = DungeonMaster()
+    dm.startGame()
+
+
     
         

d5344f37fd313f5523bb734f555200e2a6392068
Mr615-TN <tneema501@gmail.com>
2024-09-18 19:52:50 -0500

    something is getting updated

---
+++ AI_DND_DM/main.py
@@ -0,0 +1,6 @@
+from story_gen import EnhancedStoryGenerator
+from combat_sys import DetailedCombatSys, Creatue
+from inventory_sys import Character, Item, Weapon, Armor
+from npc_interaction import NPCInteraction, NPCS 
+from save_system import SaveLoadSys
+
--- AI_DND_DM/story_gen.py
+++ AI_DND_DM/story_gen.py
@@ -1,2 +1,21 @@
 import random
 from typing import List, Dict
+
+class QuestTemplate:
+    def __init__(self, name: str, description: str, objectives: List[str], difficulty: int):
+        self.name = name
+        self.description = description 
+        self.objectives = objectives 
+        self.difficulty = difficulty 
+
+class EnhancedStoryGenerator:
+    def __init__(self):
+        self.quest_template = [
+            QuestTemplate(
+                "The Lost Artifact",
+                "An ancient artifact of immense power has been stolen from the city hall",
+                ["Investigate city hall", "Track the thieves down", "Retrieve artifact"],
+                3
+            )
+        ]
+
